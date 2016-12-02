c335bcb537b8da2ab1de1b1f1ee6f2ff39256247
test <test.example.com>
2016-12-02 11:59:10 +0530

    Added Shell Scripts to install Puppet

---
+++ terraform/agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No hostname supplied"
+    exit
+fi
+env=$1
+echo $env
+hostname=`hostname`
+apt-get update -y
+apt-get upgrade -y
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt update -y
+apt-get -y install puppet
+sed -i "s/$hostname/$env/g" /etc/hostname
+sed -i "s/no/yes/g"  /etc/default/puppet
+echo "172.31.52.149  puppet.example.net    puppet" >>  /etc/hosts
+echo "172.31.52.149  puppet" >>  /etc/hosts
+echo "172.31.48.231 agent" >>  /etc/hosts
+puppet agent --waitforcert 60
+reboot
---
+++ terraform/master.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No hostname supplied"
+    exit
+fi
+env=$1
+echo $env
+hostname=`hostname`
+apt-get update -y
+apt-get upgrade -y
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt update -y
+apt-get -y install puppetmaster
+sed -i "s/$hostname/$env/g" /etc/hostname
+sed -i "s/no/yes/g"  /etc/default/puppetmaster
+echo "127.0.0.1  puppet.example.net    puppet" >>  /etc/hosts
+echo "172.31.52.149  puppet.example.net    puppet" >>  /etc/hosts
+reboot
