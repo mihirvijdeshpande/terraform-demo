b563d3a8df7322b00c70fa96b5af03c0767d94d2
Mihir Deshpande <Mihir Deshpande>
2016-12-03 19:53:40 +0530

    Removed Variables from master

--- terraform/master.sh
+++ terraform/master.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-env="puppet"
-echo $env
-hostname=`hostname`
 apt-get update -y
 apt-get upgrade -y
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
 dpkg -i puppetlabs-release-pc1-trusty.debapt update -y
 apt-get -y install puppetmaster
-sed -i "s/$hostname/$env/g" /etc/hostname
+sed -i "s/hostname/puppet/g" /etc/hostname
 sed -i "s/no/yes/g"  /etc/default/puppetmaster
 echo "127.0.0.1  puppet.example.net    puppet" >>  /etc/hosts
 echo "172.31.52.149  puppet.example.net    puppet" >>  /etc/hosts
