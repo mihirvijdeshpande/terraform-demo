34e8ecf7146dfd517fd0eccd17369de9b2e99282
Mihir Deshpande <Mihir Deshpande>
2016-12-04 08:28:45 +0530

    Reverted master to use variables. Trying to fix hostname

--- terraform/master.sh
+++ terraform/master.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+env='puppet'
+echo $env
+hostname=`hostname`
 apt-get update -y
 apt-get upgrade -y
-wget https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
-dpkg -i puppetlabs-release-pc1-trusty.debapt update -y
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt update -y
 apt-get -y install puppetmaster
-sed -i "s/hostname/puppet/g" /etc/hostname
+sed -i "s/$hostname/$env/g" /etc/hostname
 sed -i "s/no/yes/g"  /etc/default/puppetmaster
 echo "127.0.0.1  puppet.example.net    puppet" >>  /etc/hosts
-echo "172.31.52.149  puppet.example.net    puppet" >>  /etc/hosts
-echo "172.31.48.231 agent" >>  /etc/hosts
 reboot
